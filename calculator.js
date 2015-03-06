8c793562fd586618cf52f4ae1ea17a6d9760c49d
Greg Santikian <gsantikian@gmail.com>
2015-03-05 20:37:45 -0500

    Creates file and adds math functions

---
+++ calculator.js
@@ -0,0 +1,21 @@
+var sget = require('sget');
+
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+function squareRoot(num) {
+  return Math.sqrt(num);
+}
