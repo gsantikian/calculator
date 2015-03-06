e128a0da60d4b4aefc1ae1869b58a7c22c070bc8
Greg Santikian <gsantikian@gmail.com>
2015-03-05 21:45:06 -0500

    Adds loop and validates user input

--- calculator.js
+++ calculator.js
@@ -20,30 +20,62 @@ function squareRoot(num) {
   return Math.sqrt(num);
 }
 
-var userSelection = sget("Please select from the list (Enter number):\n1. Add\n2. Subtract\n3. Multiply\n4. Divide\n5. Square root\n6. Quit");
+function getValidNumber() {
+  var num = sget("Enter number: ");
+  if (isValidNumber(num)) {
+    return Number(num);
+  } else {
+    return getValidNumber();
+  }
+}
 
-switch (Number(userSelection)) {
-  case 1:
-    console.log("Add");
-    break;
+function isValidNumber(input) {
+  return Number(input) == input;
+}
 
-  case 2:
-    console.log("Subtract");
-    break;
+function performAnotherCalc() {
+  var userInput = sget("Would you like to perform another calculation? (y/n)");
+  userInput = userInput.trim();
+  if (userInput === 'y') {
+    return true;
+  } else {
+    return false;
+  }
+}
 
-  case 3:
-    console.log("Multiply");
-    break;
 
-  case 4:
-    console.log("Divide");
-    break;
+var run = true;
 
-  case 5:
-    console.log("Square Root");
-    break;
+while (run) {
+  var userSelection = sget("Please select from the list (Enter number):\n1. Add\n2. Subtract\n3. Multiply\n4. Divide\n5. Square root\n6. Quit");
 
-  case 6:
-    console.log("Goodbye");
-    break;
+  switch (Number(userSelection)) {
+    case 1:
+      console.log("Add");
+      var num1 = getValidNumber();
+      var num2 = getValidNumber();
+      console.log("%s + %s = %s", num1, num2, add(num1, num2));
+      run = performAnotherCalc();
+      break;
+
+    case 2:
+      console.log("Subtract");
+      break;
+
+    case 3:
+      console.log("Multiply");
+      break;
+
+    case 4:
+      console.log("Divide");
+      break;
+
+    case 5:
+      console.log("Square Root");
+      break;
+
+    case 6:
+      console.log("Goodbye");
+      break;
+  }
 }
