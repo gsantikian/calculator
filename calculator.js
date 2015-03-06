adbd8a00ef13d0b226290434cbb7634edb8a5fe1
Greg Santikian <gsantikian@gmail.com>
2015-03-05 20:52:34 -0500

    Adds switch statment and userInput

--- calculator.js
+++ calculator.js
@@ -19,3 +19,31 @@ function divide(num1, num2) {
 function squareRoot(num) {
   return Math.sqrt(num);
 }
+
+var userSelection = sget("Please select from the list (Enter number):\n1. Add\n2. Subtract\n3. Multiply\n4. Divide\n5. Square root\n6. Quit");
+
+switch (Number(userSelection)) {
+  case 1:
+    console.log("Add");
+    break;
+
+  case 2:
+    console.log("Subtract");
+    break;
+
+  case 3:
+    console.log("Multiply");
+    break;
+
+  case 4:
+    console.log("Divide");
+    break;
+
+  case 5:
+    console.log("Square Root");
+    break;
+
+  case 6:
+    console.log("Goodbye");
+    break;
+}
